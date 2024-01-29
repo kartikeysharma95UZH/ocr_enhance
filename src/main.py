40c0115770472d3fc7c6df0e565e8619019f1460
kartikeysharma95UZH <kartikey.sharma95@gmail.com>
2024-01-29 12:05:55 +0000

    clean up 2

--- src/main.py
+++ src/main.py
@@ -7,13 +7,13 @@ from fcr.train_set_fcr import create_train_set_fcr
 from fcr.train_fcr import train_model_fcr
 from fcr.test_fcr import test_model_fcr
 from enhance.alto_improve import improve_alto
-from ocr.train.create_pairs import create_train_pairs
-from ocr.train.train_ocr import train_ocr_model
-from ocr.test.test_ocr import test_on_set
+# from ocr.train.create_pairs import create_train_pairs
+# from ocr.train.train_ocr import train_ocr_model
+# from ocr.test.test_ocr import test_on_set
 from ocr.pipe.apply import apply_on_images
-from seg.test_seg import test_segmentation
-from epr.train_epr import train_epr_model
-from epr.test_epr import test_epr_model
+# from seg.test_seg import test_segmentation
+# from epr.train_epr import train_epr_model
+# from epr.test_epr import test_epr_model
 
 # reads config.ini to change some constants
 def read_config():
@@ -64,105 +64,14 @@ def read_config():
     ct.P7 = float(configP.get(section, "p7"))
     ct.P8 = int(configP.get(section, "p8"))
 
-
-# simply appends a slash to the end of a directory path
-def append_slash(directory):
-    if directory == None:
-        return
-    if not directory.endswith('/'):
-        directory += '/'
-    return directory
-
-# set-ocr action
-def set_ocr(args):
-    create_train_pairs(
-        args.jsonl,
-        int(args.confidence),
-        args.set,
-        append_slash(args.existing),
-        int(args.nlines),
-        int(args.generated),
-        args.text,
-        args.model
-    )
-
-# # train-ocr action
-# def train_ocr(args):
-#     train_ocr_model(
-#         args.set,
-#         args.font,
-#         args.model,
-#     )
-
-# # test-ocr action
-# def test_ocr(args):
-#     test_on_set(
-#         args.jsonl,
-#         args.image,
-#         args.confidence
-#     )
-
 #enhance action
 def enhance(args):
     improve_alto(args.directory, args.required)
 
-# ocr action
-def ocr(args):
-    apply_on_images(
-        append_slash(args.directory),
-        args.alto,
-        args.image,
-        args.confidence
-    )
-
-# set-fcr action
-def set_fcr(args):
-    create_train_set_fcr(
-        args.jsonl,
-        int(args.nchars),
-        args.set
-    )
-
-# # train-fcr action
-# def train_fcr(args):
-#     model_name = args.model
-#     if not model_name.endswith('.h5'):
-#         model_name += '.h5'
-#     train_model_fcr(
-#         args.set,
-#         model_name
-#     )
-
-# # test-fcr action
-# def test_fcr(args):
-#     test_model_fcr(
-#         args.jsonl,
-#         args.model
-#     )
-
-# # test-seg action
-# def test_seg(args):
-#     test_segmentation(
-#         args.jsonl
-#     )
-
-# # train-epr action
-# def train_epr(args):
-#     train_epr_model(
-#         args.jsonl,
-#         args.model
-#     )
-
-# # test-epr action
-# def test_epr(args):
-#     test_epr_model(
-#         args.model
-#     )
-
 ############################## start ##############################
-print("\nStarting Nautilus-OCR\n")
+print("\nStarting OCR Enhancement \n")
 time.sleep(0.5)
-parser_desc = "Nautilus-OCR Command Line Tool"
+parser_desc = "OCR Enhancement Command Line Tool"
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=parser_desc)
 subs = parser.add_subparsers(help='sub-command help')
 
